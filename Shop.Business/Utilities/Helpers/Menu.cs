fa918a2646c17666bf6e8453b4d7be8e8ff66a6d
Elkhan Mammadli <elkhantma@code.edu.az>
2024-02-05 06:42:24 +0400

    Created Menu

---
+++ Shop.Business/Utilities/Helpers/Menu.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Business.Utilities.Helpers;
+
+internal enum Menu
+{
+    //User Service
+    CreateUser =1,
+    UpdateUser,
+    DeleteUser,
+    GetAllUsers,
+    GetUserByEmail,
+    ActivateUser,
+    DeactivateUser,
+    //Card Service
+    CreateCard,
+    UpdateCard,
+    DeleteCard,
+    GetCardBalance,
+    IncreaseCardBalance,
+    DecreaseCardBalance,
+    CardExists,
+    GetCardById,
+    GetAllCards,
+    //WalletService
+    GetWalletBalance,
+    IncreaseWalletBalance,
+    //ProductService
+    CreateProduct,
+    UpdateProduct,
+    DeleteProduct,
+    ProductExists,
+    GetProductById,
+    GetAllProducts,
+    //InvoiceItemService
+    CreateInvoiceItem,
+    GetInvoiceItems,
+    CalculateTotal,
+    //InvoiceService
+    CreateInvoice,
+    UpdateInvoice,
+    DeleteInvoice,
+    GetInvoicesByUser,
+    GetInvoicesById,
+    GetAllInvoices,
+    //DeliveryAddressService
+    CreateDeliveryAddress,
+    UpdateDeliveryAddress,
+    DeleteDeliveryAddress,
+    GetDeliveryAddressById,
+    GetAllDeliveryAddresses,
+    //CategoryService
+    CreateCategory,
+    UpdateCategory,
+    DeleteCategory,
+    GetAllCategories,
+    GetCategoryById,
+    //BasketService
+    AddToBasket,
+    UpdateBasketItem,
+    RemoveFromBasket,
+    GetBasketItems,
+    CalculateTotalBasket,
+    //DiscountService
+    CreateDiscount,
+    UpdateDiscount,
+    DeleteDiscount,
+    GetDiscountById,
+    GetAllDiscounts,
+    //BrandService
+    CreateBrand,
+    UpdateBrand,
+    DeleteBrand,
+    BrandExists,
+    GetBrandById,
+    GetAllBrands
+}
